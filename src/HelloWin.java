e6c903100d51377030f3f64da44a578206f57de7
VadimBG <infotourbg.vadim@gmail.com>
2020-05-17 13:15:19 +0300

    Test 1.1 Project created in windows 10

--- src/HelloWin.java
+++ src/HelloWin.java
@@ -1,5 +1,7 @@
 public class HelloWin {
     public static void main(String[] args) {
         System.out.println("Hello Win!");
+        System.out.println("Hello Win!");
+        System.out.println("Hello Win!");
     }
 }
