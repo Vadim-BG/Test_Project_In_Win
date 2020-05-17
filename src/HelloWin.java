e57d371d3c3daa175ac2935b8184700d05e7c6f0
Vadim-BG <infotourbg.vadim@gmail.com>
2020-05-17 13:37:47 +0300

    update class

--- src/HelloWin.java
+++ src/HelloWin.java
@@ -1,11 +1,6 @@
 public class HelloWin {
     public static void main(String[] args) {
         System.out.println("Hello Win!");
-        System.out.println("Hello Win!");
-        System.out.println("Hello Win!");
-        System.out.println("Hello Win!");
-        System.out.println("Hello Win!");
-        System.out.println("Hello Win!");
-        System.out.println("Hello Win!");
+
     }
 }
